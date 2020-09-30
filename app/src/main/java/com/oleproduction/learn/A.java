d7bcaa0a099936cb6595f9cbdaf0c40d969061b6
OshanLakshitha <56622283+OshanLakshitha@users.noreply.github.com>
2020-09-30 14:16:09 +0530

    Interdoction Static nexted class

--- app/src/main/java/com/oleproduction/learn/A.java
+++ app/src/main/java/com/oleproduction/learn/A.java
@@ -1,4 +1,17 @@
 package com.oleproduction.learn;
 
 public class A {
+    static int x=10;
+    static class B{
+        static int x=20;
+    }
+    static class C{
+        static int x=30;
+    }
+    public static void main(String args[]){
+        System.out.println(A.x);
+        System.out.println(A.B.x);
+        System.out.println(A.B.x);
+    }
 }
+
